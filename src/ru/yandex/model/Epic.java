da601d419b81d1c66382f1c890f2136fe0632558
anechkarr <100240972+anechkarr@users.noreply.github.com>
2022-03-28 23:26:33 +0300

    Add files via upload

---
+++ src/ru/yandex/model/Epic.java
@@ -0,0 +1,26 @@
+package ru.yandex.model;
+
+import java.util.ArrayList;
+
+public class Epic extends Task {
+    private ArrayList<Subtask> subtasks;
+
+    public Epic(String taskName, int id, String descriptionTask, Status status) {
+        super(taskName, id, descriptionTask, status);
+    }
+
+    public void putSubtask(Subtask subtask) {
+        if (subtasks == null) {
+            subtasks = new ArrayList<>();
+        }
+        subtasks.add(subtask);
+    }
+
+    public ArrayList<Subtask> getSubtasks() {
+        return subtasks;
+    }
+
+    public void setSubtasks(ArrayList<Subtask> subtasks) {
+        this.subtasks = subtasks;
+    }
+}

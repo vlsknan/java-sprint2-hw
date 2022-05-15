1c42ace7f60ebac34ea772ec503d64b0c55fa718
anechkarr <100240972+anechkarr@users.noreply.github.com>
2022-05-15 17:35:30 +0300

    Add files via upload

--- src/ru/yandex/Main.java
+++ src/ru/yandex/Main.java
@@ -1,55 +1,40 @@
 package ru.yandex;
 
-import ru.yandex.manager.Managers;
-import ru.yandex.manager.TaskManager;
+import ru.yandex.manager.*;
 import ru.yandex.model.*;
 
 public class Main {
         public static void main(String[] args) {
 
-                TaskManager manager = Managers.getDefault("resources/task.csv");
+            TaskManager manager = Managers.getDefault("resources/task.csv");
 
-                Task task1 = new Task("Задача 1", 0, "Собрать вещи для переезда", Status.NEW, TypeTask.TASK);
-                manager.createTask(task1);
-                Task task2 = new Task("Задача 2", 0, "-", Status.IN_PROGRESS, TypeTask.TASK);
-                manager.createTask(task2);
-                System.out.println("History: " + manager.getHistory());
+            Task task1 = new Task("Задача 1", 0, "Описание задачи 1", Status.NEW, TypeTask.TASK);
+            manager.createTask(task1);
+            Task task2 = new Task("Задача 2", 0, "Описание задачи 2", Status.IN_PROGRESS, TypeTask.TASK);
+            manager.createTask(task2);
 
-                Epic epic1 = new Epic("Эпик 1", 0, "Выучить все экзамены", Status.NO, TypeTask.EPIC);
-                manager.createEpic(epic1);
+            Epic epic1 = new Epic("Эпик 1", 0, "Описание эпика 1", Status.NO, TypeTask.EPIC);
+            manager.createEpic(epic1);
+            Epic epic2 = new Epic("Эпик 2", 0, "Описание эпика 2", Status.NO, TypeTask.EPIC);
+            manager.createEpic(epic2);
 
-                Subtask subtaskForEpic1_1 = new Subtask("Мат анализ", 0, "-", Status.NEW,
-                        epic1.getID(), TypeTask.SUBTASK);
-                Subtask subtaskForEpic1_2 = new Subtask("Философия", 0, "Осталось выучить 3 билета",
-                        Status.IN_PROGRESS, epic1.getID(), TypeTask.SUBTASK);
-                Subtask subtaskForEpic1_3 = new Subtask("Помыть полы", 0, "пусть будет чисто",
-                        Status.DONE, epic1.getID(), TypeTask.SUBTASK);
-                manager.createSubtask(subtaskForEpic1_1);
-                manager.createSubtask(subtaskForEpic1_2);
-                manager.createSubtask(subtaskForEpic1_3);
+            Subtask subtaskForEpic1_1 = new Subtask("Подзадача 1", 0,
+                    "Описание подзадачи 1 эпика 1", Status.NEW, epic1.getID(), TypeTask.SUBTASK);
+            Subtask subtaskForEpic2_1 = new Subtask("Подзадача 2", 0,
+                    "Описание подзадачи 1 эпика 2", Status.IN_PROGRESS, epic2.getID(), TypeTask.SUBTASK);
+            manager.createSubtask(subtaskForEpic1_1);
+            manager.createSubtask(subtaskForEpic2_1);
 
-                Epic epic2 = new Epic("Эпик 2", 0, "-", Status.NO, TypeTask.EPIC);
-                manager.createEpic(epic2);
+            manager.getTaskByID(task1.getID());
+            manager.getEpicByID(epic1.getID());
+            manager.getTaskByID(task2.getID());
+            manager.getEpicByID(epic2.getID());
 
-                manager.getTaskByID(task2.getID());
-                System.out.println("HISTORY: " + manager.getHistory());
-                manager.getEpicByID(epic2.getID());
-                System.out.println("HISTORY: " + manager.getHistory());
-                manager.getTaskByID(task1.getID());
-                System.out.println("HISTORY: " + manager.getHistory());
-                manager.getTaskByID(task2.getID());
-                System.out.println("HISTORY: " + manager.getHistory());
-                manager.getEpicByID(epic1.getID());
-                System.out.println("HISTORY: " + manager.getHistory());
-                manager.getEpicByID(epic2.getID());
-                System.out.println("HISTORY: " + manager.getHistory());
+            TaskManager manager2 = FileBackedTasksManager.loadFromFile("resources/task.csv");
 
-                manager.deleteTaskByID(task2.getID());
-                System.out.println("HISTORY: " + manager.getHistory());
-                manager.deleteEpicByID(epic1.getID());
-                System.out.println("HISTORY: " + manager.getHistory());
-
-                System.out.println(manager.getAllEpic());
-                System.out.println(manager.getAllTasks());
+            System.out.println(manager2.getAllTasks());
+            System.out.println(manager2.getAllEpic());
+            System.out.println(manager2.getAllSubtasks());
+            System.out.println(manager2.getHistory());
         }
 }
